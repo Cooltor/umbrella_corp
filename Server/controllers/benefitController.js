9ccbd46e57d9fee0bcdf05423cbdbecab85758bd
Cooltor <romaindbus86@gmail.com>
2023-01-25 16:42:38 +0100

    errorController dev and prod

--- Server/controllers/benefitController.js
+++ Server/controllers/benefitController.js
@@ -1,7 +1,7 @@
-const { json } = require("express");
 const APIFeatures = require("../utils/apiFeatures");
 const catchAsync = require("../utils/catchAsync");
 const Benefit = require("../models/benefitModel");
+const AppError = require("../utils/appError");
 
 exports.getAllBenefits = catchAsync(async (req, res, next) => {
   //EXECUTE THE QUERY
@@ -24,6 +24,10 @@ exports.getAllBenefits = catchAsync(async (req, res, next) => {
 
 exports.getBenefit = catchAsync(async (req, res, next) => {
   const benefit = await Benefit.findById(req.params.id);
+
+  if (!benefit) {
+    return next(new AppError("No Tour found with that ID", 404));
+  }
   // Tour.findOne({_id: req.params.id})   ==> méthode pour find 1 seul doc : équivalent a findById
   res.status(200).json({
     status: "succes",
@@ -34,9 +38,6 @@ exports.getBenefit = catchAsync(async (req, res, next) => {
 });
 
 exports.createBenefit = catchAsync(async (req, res, next) => {
-  //const newTour = new Tour({});
-  //newTour.save();
-
   const newBenefit = await Benefit.create(req.body); //voir pages
 
   res.status(201).json({
@@ -55,6 +56,11 @@ exports.updateBenefit = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+
+  if (!benefit) {
+    return next(new AppError("No Tour found with that ID", 404));
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -64,8 +70,11 @@ exports.updateBenefit = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteBenefit = catchAsync(async (req, res, next) => {
-  await Benefit.findByIdAndDelete(req.params.id);
+  const benefit = await Benefit.findByIdAndDelete(req.params.id);
 
+  if (!benefit) {
+    return next(new AppError("No Tour found with that ID", 404));
+  }
   res.status(204).json({
     status: "success",
     data: null,
