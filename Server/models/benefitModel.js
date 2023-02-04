7124dea813248b191be1c4dd8f69769e2c7bde0b
Cooltor <romaindbus86@gmail.com>
2023-02-04 13:10:05 +0100

    virtual populate reviews ok

--- Server/models/benefitModel.js
+++ Server/models/benefitModel.js
@@ -1,60 +1,69 @@
 const mongoose = require("mongoose");
 // const slugify = require("slugify");
 
-const benefitSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: [true, "Une prestation doit avoir un type"],
-    trim: true,
-    maxlength: [
-      40,
-      "Un type de prestation doir avoir un maximum de 40 caractères",
-    ],
-    minlength: [
-      10,
-      "Un type de prestation doir avoir un minimum de 10 caractères",
-    ],
+const benefitSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: [true, "Une prestation doit avoir un type"],
+      trim: true,
+      maxlength: [
+        40,
+        "Un type de prestation doir avoir un maximum de 40 caractères",
+      ],
+      minlength: [
+        10,
+        "Un type de prestation doir avoir un minimum de 10 caractères",
+      ],
+    },
+    title: {
+      type: String,
+      unique: true,
+      required: [true, "Une prestation doit avoir un titre"],
+      maxlength: [
+        40,
+        "Un titre de prestation doir avoir un maximum de 40 caractères",
+      ],
+      minlength: [
+        10,
+        "Un titre de prestation doir avoir un minimum de 10 caractères",
+      ],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "Une prestation doit avoir une description"],
+      maxlength: [
+        255,
+        "Un description doir avoir un maximum de 255 caractères",
+      ],
+      minlength: [50, "Un description doir avoir un minimum de 50 caractères"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: [true, "Une prestation doit avoir un prix"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
   },
-  title: {
-    type: String,
-    unique: true,
-    required: [true, "Une prestation doit avoir un titre"],
-    maxlength: [
-      40,
-      "Un titre de prestation doir avoir un maximum de 40 caractères",
-    ],
-    minlength: [
-      10,
-      "Un titre de prestation doir avoir un minimum de 10 caractères",
-    ],
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, "Une prestation doit avoir une description"],
-    maxlength: [255, "Un description doir avoir un maximum de 255 caractères"],
-    minlength: [50, "Un description doir avoir un minimum de 50 caractères"],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, "Rating must be above 1.0"],
-    max: [5, "Rating must be below 5.0"],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 1,
-  },
-  price: {
-    type: Number,
-    required: [true, "Une prestation doit avoir un prix"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 // Virtual populate reviews
 benefitSchema.virtual("reviews", {
