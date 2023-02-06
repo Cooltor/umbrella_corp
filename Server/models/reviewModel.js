051e8ebcb28f58712ea6e985e76ba1acbb8f2832
Cooltor <romaindbus86@gmail.com>
2023-02-06 22:49:59 +0100

    review update delete

--- Server/models/reviewModel.js
+++ Server/models/reviewModel.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const Benefit = require("./benefitModel");
 
 const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
-      required: [true, "Review cannot be empty"],
+      required: [true, "Un commentaire ne peut pas être vide."],
     },
     rating: {
       type: Number,
@@ -18,12 +19,12 @@ const reviewSchema = new mongoose.Schema(
     benefit: {
       type: mongoose.Schema.ObjectId,
       ref: "Benefit",
-      required: [true, "Review must belong to a Benefit."],
+      required: [true, "Un commentaire doit être relié à une prestation."],
     },
     user: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
-      required: [true, "Review must belong to a user."],
+      required: [true, "Un commentaire doit être relié à un utilisateur."],
     },
   },
   {
@@ -44,6 +45,40 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (benefitId) {
+  const stats = await this.aggregate([
+    {
+      $match: { benefit: benefitId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  const ratingsQuantity = stats[0]?.numRatings ?? 0;
+  const ratingsAverage = stats[0]?.avgRating ?? 4.5;
+
+  await Benefit.findByIdAndUpdate(benefitId, {
+    ratingsQuantity,
+    ratingsAverage,
+  });
+};
+
+reviewSchema.post("save", function () {
+  //this points to current review
+
+  this.constructor.calcAverageRatings(this.benefit); /// contructor vise vers le model actuel
+});
+
+reviewSchema.post(/^findOneAnd/, async (doc) => {
+  // Affiche zéro pour la quantité de note
+  await doc.constructor.calcAverageRatings(doc.benefit);
+});
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
