6c83b7ec03ff52efeda4f67b944fa51dac3a9a6b
Cooltor <romaindbus86@gmail.com>
2023-02-07 10:26:16 +0100

    ratings average math.round

--- Server/models/benefitModel.js
+++ Server/models/benefitModel.js
@@ -44,6 +44,7 @@ const benefitSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, "La note doit être minimum de 1.0"],
       max: [5, "La note doit être maximum de 5.0"],
+      set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.66666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
