c99e9bc6cfb7aed4bf03d93a709fc1c418d0ff0f
Cooltor <romaindbus86@gmail.com>
2023-02-17 10:27:41 +0100

    email handler

--- Server/utils/email.js
+++ Server/utils/email.js
@@ -1,30 +1,62 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
-const sendEmail = async (options) => {
-  // 1. Create a transporter
-  const transporter = nodemailer.createTransport({
-    //service: "Gmail",
-    host: process.env.EMAIL_HOST, // for mailtrap
-    port: process.env.EMAIL_PORT, // for Mailtrap
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activate in gmail "less secure app" option
-    // Gmail limite à 100 envois par jour et peut nous mettre ds span donc c'est pas bon pour un site (grand) public
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.firstname;
+    this.url = url;
+    this.from = `Umbrella Corp. <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2. Define the email options
-  const mailOptions = {
-    from: "Romain Dubus <romaindubus86@gmail.com",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
 
-  // 3. Actually send the email
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport and send email
+    this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Bienvenue chez Umbrella Corp."); // c'est la class qui s'occupera de l'implémentation
+  }
 };
-
-module.exports = sendEmail;
